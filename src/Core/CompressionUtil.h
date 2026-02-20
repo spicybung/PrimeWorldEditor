f19c407d4b282603d365f9b99650b8a97bd727d6
Lioncache <mai.iam2048@gmail.com>
2026-02-20 14:28:46 -0500

    CompressionUtil: Fix prototypes for CompressLZO/DecompressLZO
    Now these will resolve properly if ever used.

--- src/Core/CompressionUtil.h
+++ src/Core/CompressionUtil.h
@@ -7,12 +7,12 @@ namespace CompressionUtil
 {
     // Decompression
     bool DecompressZlib(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t DstLen, uint32_t& rTotalOut);
-    bool DecompressLZO(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t& rTotalOut);
+    bool DecompressLZO(uint8_t* pSrc, uint32_t SrcLen, uint8_t* pDst, uint32_t DstLen, uint32_t& rTotalOut);
     bool DecompressSegmentedData(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t DstLen);
 
     // Compression
     bool CompressZlib(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t DstLen, uint32_t& rTotalOut);
-    bool CompressLZO(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t& rTotalOut);
+    bool CompressLZO(uint8_t* pSrc, uint32_t SrcLen, uint8_t* pDst, uint32_t DstLen, uint32_t& rTotalOut);
     bool CompressSegmentedData(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t& rTotalOut, bool IsZlib, bool AllowUncompressedSegments);
     bool CompressZlibSegmented(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t& rTotalOut, bool AllowUncompressedSegments);
     bool CompressLZOSegmented(uint8_t *pSrc, uint32_t SrcLen, uint8_t *pDst, uint32_t& rTotalOut, bool AllowUncompressedSegments);
