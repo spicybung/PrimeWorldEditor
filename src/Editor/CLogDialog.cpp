bb03bd8793955725fe04b23a15b715cc43d3f808
Lioncache <mai.iam2048@gmail.com>
2026-02-27 17:33:33 -0500

    CLogDialog: Add button to clear log
    Just a small convenience thing.

--- src/Editor/CLogDialog.cpp
+++ src/Editor/CLogDialog.cpp
@@ -13,6 +13,9 @@ CLogDialog::CLogDialog(QWidget *pParent)
 {
     ui->setupUi(this);
     connect(ui->CloseButton, &QPushButton::clicked, this, &CLogDialog::close);
+    connect(ui->ClearLogButton, &QPushButton::clicked, [this] {
+        ui->LogTextEdit->clear();
+    });
 
     constexpr int max_lines = 300;
     NLog::Get()->sinks().push_back(std::make_shared<spdlog::sinks::qt_color_sink_mt>(ui->LogTextEdit, max_lines, false, true));
