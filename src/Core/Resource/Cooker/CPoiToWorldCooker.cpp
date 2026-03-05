4516cab802a38519c66bd0c0ac474bf3ad17e835
Lioncache <mai.iam2048@gmail.com>
2026-03-04 21:19:35 -0500

    CPoiToWorldCooker: Pass most tests for Echoes

--- src/Core/Resource/Cooker/CPoiToWorldCooker.cpp
+++ src/Core/Resource/Cooker/CPoiToWorldCooker.cpp
@@ -1,6 +1,8 @@
 #include "Core/Resource/Cooker/CPoiToWorldCooker.h"
 
 #include "Core/Resource/CPoiToWorld.h"
+#include <algorithm>
+#include <compare>
 #include <vector>
 
 bool CPoiToWorldCooker::CookEGMC(const CPoiToWorld* pPoiToWorld, IOutputStream& rOut)
@@ -10,6 +12,8 @@ bool CPoiToWorldCooker::CookEGMC(const CPoiToWorld* pPoiToWorld, IOutputStream& 
     {
         uint32_t MeshID{};
         CInstanceID PoiID;
+
+        constexpr auto operator<=>(const SPoiMapping&) const noexcept = default;
     };
     std::vector<SPoiMapping> Mappings;
 
@@ -23,9 +27,14 @@ bool CPoiToWorldCooker::CookEGMC(const CPoiToWorld* pPoiToWorld, IOutputStream& 
         }
     }
 
+    // Echoes typically sorts in ascending order
+    std::ranges::sort(Mappings);
+
     // Write EGMC
     rOut.WriteU32(static_cast<uint32_t>(Mappings.size()));
 
+    // TODO: See CPoiToWorldLoader. We currently mask out the area index from the instance ID
+    //       with & 0x03FFFFFF, which results in incorrect POI IDs being cooked.
     for (const auto& mapping : Mappings)
     {
         rOut.WriteU32(mapping.MeshID);
